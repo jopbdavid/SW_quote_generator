149ec062aba3966a19a9360e6f9f21f5a6efd29a
jopbdavid <jopbdavid@gmail.com>
2023-05-11 11:19:00 +0100

    first commit

---
+++ SW_generator.js
@@ -0,0 +1,57 @@
+const quotes = [
+  {
+    "Darth Vader": [
+      "Ive been waiting for you, Obi-Wan. We meet again, at last. The circle is now complete. When I left you, I was but the learner; now I am the master.",
+      "The Force is strong with this one.",
+      "No. I am your father.",
+      "I find your lack of faith disturbing",
+      "You have failed me for the last time.",
+    ],
+  },
+  {
+    "Obi-Wab Kenobi": [
+      "Use the Force, Luke.",
+      "You were the chosen one! It was said that you would destroy the Sith, not join them. You were to bring balance to the Force, not leave it in darkness.",
+      "In my experience, there’s no such thing as luck.",
+      "You were my brother, Anakin. I loved you.",
+      "I have a bad feeling about this.",
+    ],
+  },
+  {
+    "Han Solo": [
+      "Never tell me the odds!",
+      "Chewie, we’re home.",
+      "Great, kid. Don’t get cocky.",
+    ],
+  },
+  {
+    Yoda: [
+      "Judge me by my size, do you?",
+      "Do. Or do not. There is no try.",
+      "Fear is the path to the dark side.",
+    ],
+  },
+];
+
+var button = document.querySelector(".generate");
+
+const displayQuote = () => {
+  let index = Math.floor(Math.random() * quotes.length);
+  let key = Object.keys(quotes[index]);
+  let keyArray = quotes[index][key];
+  let secondIndex = Math.floor(Math.random() * keyArray.length);
+  let prevQuote;
+  let quote = quotes[index][key][secondIndex];
+  if (quote === prevQuote && secondIndex + 1 < keyArray.length) {
+    quote = quotes[index][key][secondIndex + 1];
+  } else if (quote === prevQuote && secondIndex + 1 === keyArray.length) {
+    quote = quotes[index][key][secondIndex - 1];
+  }
+
+  document.querySelector("#quote").innerHTML = `${quote} - "${key}"`;
+
+  prevQuote = quote;
+  console.log(secondIndex);
+};
+
+button.addEventListener("click", displayQuote);
